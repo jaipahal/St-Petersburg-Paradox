60f2e0f501f4ced9d5103f507db092010961af27
VineetAhujaX <vineetahuja208@gmail.com>
2021-11-12 18:43:01 +0530

    fixed update within loop and added flip count

--- game.py
+++ game.py
@@ -43,21 +43,34 @@ loadImage = loadImage.resize((150, 150))
 heads = ImageTk.PhotoImage(loadImage)
 
 #load tails
-loadImage = Image.open("tails.jpg")
-loadImage = loadImage.resize((150, 150))
-tails = ImageTk.PhotoImage(loadImage)
+load = Image.open("tails.jpg")
+load = load.resize((150, 150))
+tails = ImageTk.PhotoImage(load)
+
+#load unknown
+load2 = Image.open("unknown.jpg")
+load2 = load2.resize((150, 150))
+unknown = ImageTk.PhotoImage(load2)
 
 # This is the section of code which creates a image
-coinResult = Label(root, image=heads)
+coinResult = Label(root, image=unknown)
 coinResult.place(x=400, y=300)
+
+# This is the section of code which creates the a label
+Label(root, text='Flip Count: ', bg='#F0F8FF', font=('arial', 18, 'normal')).place(x=400, y=460)
+
+# This is the section of code which creates the a label
+showFlipCount = Label(root, text='0', bg='#F0F8FF', font=('arial', 18, 'normal'))
+showFlipCount.place(x=520, y=460, width= 30)
 
 
 # This is the section of code which creates the a label
-Label(root, text='Number of heads: ', bg='#F0F8FF', font=('arial', 18, 'normal')).place(x=500, y=200,)
+Label(root, text='Number of heads: ', bg='#F0F8FF', font=('arial', 18, 'normal')).place(x=500, y=200)
 
 # This is the section of code which creates the a label
 showHeadCount = Label(root, text='0', bg='#ADD8E6', font=('arial', 18, 'normal'))
 showHeadCount.place(x=700, y=200, width= 70)
+
 
 # This is the section of code which creates the a label
 Label(root, text='Amount won: ', bg='#F0F8FF', font=('arial', 18, 'normal')).place(x=500, y=250)
@@ -73,17 +86,34 @@ def startGame():
     clicksCount+=1
     start["state"] = "disabled"
     headcount = 0
+    showHeadCount.config(text= str(headcount))
+    showAmountWon.config(text= str(headcount))
+    i=0
+    root.update()
     while(True):
+        i+=1
+        showFlipCount.config(text= str(i))
         outcome=random.randint(0,1)
         if outcome == 0:
             coinResult.config(image=heads)
             headcount+=1
+            #showHeadCount.config(text= str(headcount))
+            #showAmountWon.config(text= str(2**headcount))
+            root.update()
         else:
             coinResult.config(image=tails)
+            root.update()
             break
         showHeadCount.config(text= str(headcount))
-        #showAmountWon.config(text= str(2**headcount))
-    showAmountWon.config(text= str(clicksCount))
+        showAmountWon.config(text= str(2**headcount))
+        root.update()
+        time.sleep(1)
+    time.sleep(1)
+    showFlipCount.config(text= str(0))
+    coinResult.config(image=unknown)
+    root.update()
+    #showAmountWon.config(text= str(clicksCount))
+
     start["state"] = "normal"
 
 def flip():
